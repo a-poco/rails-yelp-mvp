6f4cf0e513e43650a9caeb9d04ce09f0650e51bb
Cristina Salazar <cristinasalazarm1@gmail.com>
2021-05-19 18:45:47 +0200

    add cotrollers view for restaurant and review

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(
+  name: "La neta",
+  address: 'Stockholm',
+  phone_number: 6622005101,
+  category: "chinese")
+
+Restaurant.create(
+  name: "Fluffy",
+  address: 'Paris',
+  phone_number: 0707134413,
+  category: "italian")
+
+Restaurant.create(
+  name: "boungour",
+  address: 'french',
+  phone_number: 123456789,
+  category: "french")
+
+Restaurant.create(
+  name: "Arigato",
+  address: 'Barcelona',
+  phone_number: 6624223559,
+  category: "japanese")
+
+Restaurant.create(
+  name: "Taco Bar",
+  address: 'Mexico',
+  phone_number: 6621224567,
+  category: "belgian")
